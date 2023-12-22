41e120b8b4f91c20df6c9f09672d7be5bf441b05
Arturo Guerra <arturoalejandroguerram@gmail.com>
2023-12-22 14:27:20 -0500

    hola

--- src/server.js
+++ src/server.js
@@ -25,6 +25,16 @@ app.use(
   })
 );
 
+app.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  response.header(
+   'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+ );
+
+ next();
+})
+
 app.use("/services", serviceRouter);
 app.use("/products", productRouter);
 app.use("/users", userRouter);
